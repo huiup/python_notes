02c81e9753a5b231d1f03fd228a977c75cd8390f
huiup <1127473030@qq.com>
2020-10-10 20:20:30 +0800

    commit!!!

--- 线程/线程之间共享全局变量_05.py
+++ 线程/线程之间共享全局变量_05.py
@@ -1,2 +1,16 @@
 #  coding:utf-8
+import threading
 
+alist = []
+
+
+def add_data():
+    for i in range(5):
+        alist.append(i)
+        print(i)
+    print('添加完成：', alist)
+
+
+if __name__ == '__main__':
+    add_thread = threading.Thread(target=add_data())
+    add_thread.start()
